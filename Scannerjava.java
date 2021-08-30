0132cfec02bce218a5fdd9825e3f1f9bda485cf0
marciodesenvolvedor <89565704+marciodesenvolvedor@users.noreply.github.com>
2021-08-30 13:44:06 -0300

    Utilizando na Classe Scanner o IF/ELSE
    Aplicando a estrutura condicional if/else é um recurso que indica quais instruções o sistema deve processar de acordo com uma expressão booleana.

---
+++ Scannerjava.java
@@ -0,0 +1,19 @@
+﻿import java.util.*;
+
+public class Programa
+{
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        
+        if(N % 2 != 0)
+        {
+            System.out.println("Estranho");
+        }
+        else
+        {
+            if(N < 10)
+            {
+                System.out.println("Não é estranho");
+            }
